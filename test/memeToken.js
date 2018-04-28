0e110379fd04ba0102582aae20232ca87d39249b
Admin <rakesh@Rakeshs-MacBook-Pro.local>
2018-04-28 15:44:41 -0700

    Adding first changes

--- test/memeToken.js
+++ test/memeToken.js
@@ -5,12 +5,12 @@ describe("mint", () => {
     let instance = await MemeToken.deployed();
     let owner = await instance.owner();
 
-    let token = await instance.mint("smiley", "qwsred12345rfd");
+    let token = await instance.mint("smiley", "QmUqRKhS8GzCiFaiAQ4A9g2TFL4emCNDJYaRJa1ZByaUk7");
    // console.log(token);
 
    // let tokens = await instance.tokensOf(owner);
     let memes = await instance.getMeme(0);
     console.log(memes);
-    assert.deepEqual(memes, ["smiley", "qwsred12345rfd"]);
+    assert.deepEqual(memes, ["smiley", "QmUqRKhS8GzCiFaiAQ4A9g2TFL4emCNDJYaRJa1ZByaUk7"]);
   });
 });
